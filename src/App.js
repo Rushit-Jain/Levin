d92e675b828e7aa6bee8a365ee43a9e677a10bff
Rushit Jain <rushit07@gmail.com>
2021-08-18 23:15:20 +0530

    nav and footer done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import LNavbar from "./components/navbar/LNavbar";
-import "bootstrap/dist/css/bootstrap.css";
+import Footer from "./components/footer/Footer";
+import { Switch, Redirect, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <LNavbar />
+      <Switch>
+        {/* <Route path="/home" exact component={LNavbar} />
+        <Route path="/about" exact component={LNavbar} />
+        <Route path="/products" exact component={LNavbar} />
+        <Route path="/contact" exact component={LNavbar} /> */}
+        <Redirect to="/home" />
+      </Switch>
+      <Footer />
     </div>
   );
 }
--- src/components/navbar/LNavbar.js
+++ src/components/navbar/LNavbar.js
@@ -3,31 +3,43 @@ import { Navbar, Nav, Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./LNavbar.css";
 import logo from "../../assets/logo.png";
+import catalogue from "../../assets/August_2021_Catalogue.pdf";
 
 class LNavbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navIsExp: false,
+    };
+  }
   render() {
     return (
       <Navbar className="LNavbar" expand="lg">
         <Navbar.Brand href="/home" className="logo-box">
           <img className="logo" src={logo} alt="Levin" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          id="toggle-nav"
+          aria-controls="basic-navbar-nav"
+          className={this.state.navIsExp ? "rotate-nav" : "rotate-nav-opp"}
+          onClick={() => this.setState({ navIsExp: !this.state.navIsExp })}
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="navlinks-box">
             <NavLink className="links" to="/home">
               Home
             </NavLink>
-            <NavLink className="links" to="/link">
+            <NavLink className="links" to="/about">
               About Us
             </NavLink>
-            <NavLink className="links" to="/home">
-              Products
+            <NavLink className="links" to="/products">
+              Our Products
             </NavLink>
-            <NavLink className="links" to="/link">
+            <a className="links" target="_blank" href={catalogue}>
+              View Catalogue
+            </a>
+            <NavLink className="links" to="/contact">
               Contact Us
-            </NavLink>
-            <NavLink className="links" to="/link">
-              Download Catalogue
             </NavLink>
           </Nav>
         </Navbar.Collapse>

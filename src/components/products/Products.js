cdb4c69712260d6822ff6444f93099375e55f39d
Rushit Jain <rushit07@gmail.com>
2021-08-29 00:45:20 +0530

    categories done

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -14,20 +14,60 @@ import closed_mcb from "../../assets/product_buttons/closed_mcb.svg";
 import closed_accessories from "../../assets/product_buttons/closed_accessories.svg";
 import productsJumbotronImage from "../../assets/illustrations/products.svg";
 
-import {useHistory} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
 
 const categories = {
-  'doric':[ "Switches", "Sockets", "Ancillaries", "Dimmers & Fan Regulators", "LED Light", "Bell, LED & Hotel Series", "Motor Starters & Tiny MCBs"
-          , "AC & Geyser Box", "Switch Plates", "Surface & Concealed Box"],
-  'dolby':[ "Switches", "Sockets", "Dimmers & Fan Regulators", "Ancillaries", "Fuse, Tiny MCB & Blank Module"
-          , "Bell, LED & Hotel Series", "Switch Plates" ],
-  'dorun':[ "Switches", "Sockets", "Dimmers & Fan Regulators", "Fuse, T.V. Socket & Telephone Jack", "Double Pole Switches", "Switch & Socket Combined"
-          , "5 in 1" ],
-  'mcb':[ "Single Pole C Type", "Double Pole C Type", "Triple Pole C Type", "Four Pole & Neutral C Type", "Isolator", "Single Pole & Neutral C Type"
-          , "Triple Pole & Neutral C Type", "Changeover", "RCCB" ],
-  'accessories':[ "Ceiling Rose", "Lamp Holders", "Plug Tops & Bed Switch", "Conversion & Multi Plugs", "Door Bells", "Surface Box & Ceiling plates"
-          , "Power Strip" ]
-}
+  doric: [
+    "Switches",
+    "Sockets",
+    "Ancillaries",
+    "Dimmers & Fan Regulators",
+    "LED Light",
+    "Bell, LED & Hotel Series",
+    "Motor Starters & Tiny MCBs",
+    "AC & Geyser Box",
+    "Switch Plates",
+    "Surface & Concealed Box",
+  ],
+  dolby: [
+    "Switches",
+    "Sockets",
+    "Dimmers & Fan Regulators",
+    "Ancillaries",
+    "Fuse, Tiny MCB & Blank Module",
+    "Bell, LED & Hotel Series",
+    "Switch Plates",
+  ],
+  dorun: [
+    "Switches",
+    "Sockets",
+    "Dimmers & Fan Regulators",
+    "Fuse, T.V. Socket & Telephone Jack",
+    "Double Pole Switches",
+    "Switch & Socket Combined",
+    "5 in 1",
+  ],
+  mcb: [
+    "Single Pole C Type",
+    "Double Pole C Type",
+    "Triple Pole C Type",
+    "Four Pole & Neutral C Type",
+    "Isolator",
+    "Single Pole & Neutral C Type",
+    "Triple Pole & Neutral C Type",
+    "Changeover",
+    "RCCB",
+  ],
+  accessories: [
+    "Ceiling Rose",
+    "Lamp Holders",
+    "Plug Tops & Bed Switch",
+    "Conversion & Multi Plugs",
+    "Door Bells",
+    "Surface Box & Ceiling plates",
+    "Power Strip",
+  ],
+};
 
 class Products extends Component {
   constructor(props) {
@@ -40,27 +80,42 @@ class Products extends Component {
   handleRangeChange = (range) => this.setState({ selectedRange: range });
 
   render() {
-
-    let categoriesLayout = []
-    for(i=0;i<categories['dolby'].length;i=i+2){
-      
-      categoriesLayout.push(
-      <Row className="product-category-row">
-        <CategoryCard
-          categoryName={categories['dolby'][i]}
-          background="products-category-bg-4"
-        />
-        {
-          i % 2 !== 0 ?
-          <CategoryCard
-            categoryName={categories['dolby'][i+1]}
-            background="products-category-bg-2"
-          /> :
-          <div></div>
-        }  
-          
-      </Row>
-      )
+    let categoriesLayout = [];
+    let classIndex = 0;
+    for (
+      let i = 0;
+      i < categories[this.state.selectedRange].length;
+      i = i + 2
+    ) {
+      classIndex = classIndex % 5;
+      if (
+        categories[this.state.selectedRange].length % 2 !== 0 &&
+        i === categories[this.state.selectedRange].length - 1
+      ) {
+        categoriesLayout.push(
+          <Row className="product-category-row">
+            <CategoryCard
+              categoryName={categories[this.state.selectedRange][i]}
+              background={"products-category-bg-" + classIndex}
+            />
+          </Row>
+        );
+        classIndex += 1;
+      } else {
+        categoriesLayout.push(
+          <Row className="product-category-row">
+            <CategoryCard
+              categoryName={categories[this.state.selectedRange][i]}
+              background={"products-category-bg-" + classIndex}
+            />
+            <CategoryCard
+              categoryName={categories[this.state.selectedRange][i + 1]}
+              background={"products-category-bg-" + (classIndex + 1)}
+            />
+          </Row>
+        );
+        classIndex += 2;
+      }
     }
 
     return (
@@ -79,7 +134,10 @@ class Products extends Component {
             <Col>
               <img
                 src={productsJumbotronImage}
-                style={{ background: "antiquewhite", width: "80%" }}
+                style={{
+                  background: "antiquewhite",
+                  width: "80%",
+                }}
               />
             </Col>
           </Col>
@@ -99,7 +157,10 @@ class Products extends Component {
             }}
           >
             <img
-              onClick={() => {this.handleRangeChange("dolby") ; this.props.history.push("/products/dolby") ;  }}
+              onClick={() => {
+                this.handleRangeChange("dolby");
+                this.props.history.push("/products/dolby");
+              }}
               height="100px"
               width="200px"
               alt="Dolby"
@@ -108,7 +169,10 @@ class Products extends Component {
               }
             />
             <img
-              onClick={() => {this.handleRangeChange("doric") ; this.props.history.push("/products/doric") }}
+              onClick={() => {
+                this.handleRangeChange("doric");
+                this.props.history.push("/products/doric");
+              }}
               height="100px"
               width="200px"
               alt="Doric"
@@ -117,7 +181,10 @@ class Products extends Component {
               }
             />
             <img
-              onClick={() => {this.handleRangeChange("dorun") ; this.props.history.push("/products/dorun") }}
+              onClick={() => {
+                this.handleRangeChange("dorun");
+                this.props.history.push("/products/dorun");
+              }}
               height="100px"
               width="200px"
               alt="Dorun"
@@ -126,7 +193,10 @@ class Products extends Component {
               }
             />
             <img
-              onClick={() => {this.handleRangeChange("accessories") ; this.props.history.push("/products/accessories") }}
+              onClick={() => {
+                this.handleRangeChange("accessories");
+                this.props.history.push("/products/accessories");
+              }}
               height="100px"
               width="200px"
               alt="Accessories"
@@ -137,7 +207,10 @@ class Products extends Component {
               }
             />
             <img
-              onClick={() => {this.handleRangeChange("mcb") ; this.props.history.push("/products/mcb") }}
+              onClick={() => {
+                this.handleRangeChange("mcb");
+                this.props.history.push("/products/mcb");
+              }}
               height="100px"
               width="200px"
               alt="MCB"
@@ -145,12 +218,8 @@ class Products extends Component {
             />
           </Row>
         </Container>
-        <>
-        {
-          categoriesLayout
-        }
-        </>
-          {/* <Row className="product-category-row">
+        <>{categoriesLayout}</>
+        {/* <Row className="product-category-row">
                   <CategoryCard
                     categoryName="AC &amp; Geyser Box"
                     background="products-category-bg-1"

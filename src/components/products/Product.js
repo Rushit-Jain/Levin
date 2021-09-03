bb509de32b67e6f8e8ec2646a4533a336c054163
Bhavesh Bellaney <bhaveshbellaney@gmail.com>
2021-09-03 12:01:49 +0530

    new card design done

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -6,53 +6,75 @@ import pic_svg from "../../assets/pic.svg";
 
 function Product(props) {
   return (
-    <div className="product-wrapper" style={{ height: "60vh" }}>
-      <Goo composite intensity="strong">
-        <div className="product-card">
-          <div className="product-img-div">
-            <img className="product-img" src={"data:image/png;base64," + props.data.image} alt="PRODUCT-IMAGE" />
-          </div>
-          <h1 className="product-h1-class">{props.data.name}</h1>
-          <small className="product-small-class">{props.data.code}</small>
+    // <div className="product-wrapper" style={{ height: "60vh" }}>
+    //   <Goo composite intensity="strong">
+    //     <div className="product-card">
+    //       <div className="product-img-div">
+    //         <img className="product-img" alt="PRODUCT-IMG" src={"data:image/png;base64," + props.data.image} />
+    //       </div>
+    //       <h1 className="product-h1-className">{props.data.name}</h1>
+    //       <small className="product-small-className">{props.data.code}</small>
 
-          <label className="product-label-class">
-            <input className="product-input-class" type="checkbox" />
-            <span className="product-span-class">
-              <i className="fas fa-chevron-down"></i>
-            </span>
+    //       <label className="product-label-className">
+    //         <input className="product-input-className" type="checkbox" />
+    //         <span className="product-span-className">
+    //           <i className="fas fa-chevron-down"></i>
+    //         </span>
 
-            <div className="product-dets-class">
+    //         <div className="product-dets-className">
 
-              <Row>
-                <Col xs={2}>Color:</Col>
-                <Col>{props.data.color.map((c, index) => {
-                  if (index == 0) {
-                    return c
-                  }
-                  // if(props.data.color.length-1==index){
-                  //   return ", "+c
-                  // }
-                  else {
-                    return ", " + c
-                  }
-                })}</Col>
-              </Row>
-              <Row>
-                <Col xs={2}>Code:</Col>
-                <Col>DX-00000</Col>
-              </Row>
-              <Row>
-                <Col xs={2}>Types:</Col>
-                <Col> 1M, 2M, 3M, 4M, 5M, 6M, 7M, 8M, 9M, 10M</Col>
-              </Row>
-              {/* <div>Color: Metallic Black, Metallic Grey, White</div>
-              <div>Code: DX-00000</div>
-              <div>Types: 1M, 2M, 3M, 4M, 5M, 6M, 7M, 8M, 9M, 10M</div> */}
+    //           <Row>
+    //             <Col xs={2}>Color:</Col>
+    //             <Col>{props.data.color.map((c, index) => {
+    //               if (index == 0) {
+    //                 return c
+    //               }
+    //               // if(props.data.color.length-1==index){
+    //               //   return ", "+c
+    //               // }
+    //               else {
+    //                 return ", " + c
+    //               }
+    //             })}</Col>
+    //           </Row>
+    //           <Row>
+    //             <Col xs={2}>Code:</Col>
+    //             <Col>DX-00000</Col>
+    //           </Row>
+    //           <Row>
+    //             <Col xs={2}>Types:</Col>
+    //             <Col> 1M, 2M, 3M, 4M, 5M, 6M, 7M, 8M, 9M, 10M</Col>
+    //           </Row>
+    //           {/* <div>Color: Metallic Black, Metallic Grey, White</div>
+    //           <div>Code: DX-00000</div>
+    //           <div>Types: 1M, 2M, 3M, 4M, 5M, 6M, 7M, 8M, 9M, 10M</div> */}
 
-            </div>
-          </label>
+    //         </div>
+    //       </label>
+    //     </div>
+    //   </Goo>
+    // </div>
+    <div className="card" tabIndex="0">
+      <div className="imgBx">
+        <img alt="PRODUCT-IMG" src={"data:image/png;base64," + props.data.image} />
+      </div>
+      <div className="contentBx">
+        <h2 className="product-h1-className">{props.data.name}</h2>
+        <small className="product-small-className">{props.data.code}</small>
+        <div className="size">
+          <h3>Size :</h3>
+          <span>7</span>
+          <span>8</span>
+          <span>9</span>
+          <span>10</span>
         </div>
-      </Goo>
+        <div className="color">
+          <h3>Color :</h3>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
     </div>
   );
 }

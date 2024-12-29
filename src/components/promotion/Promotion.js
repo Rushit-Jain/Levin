7325dd7b346dbd464bc301a73f510a3c813376cf
Rushit Jain <rushit07@gmail.com>
2024-12-30 00:26:24 +0530

    added onclick attribute and logic to carousel items

--- src/components/promotion/Promotion.js
+++ src/components/promotion/Promotion.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import {useHistory} from 'react-router-dom';
 import {Image} from 'react-bootstrap';
 import { Carousel } from 'react-bootstrap';
-import bedroom from '../../assets/bedroom.jpg';
 const images = require.context('../../assets/promotions', true);
 const imageList = images.keys().map(image => images(image));
 
 const Promotion = (props) => {
+    let history = useHistory();
     let carousel = imageList.map((image,i) => {
+        let index = +((image.split("/")[3]).split('\.')[0])-1;
         return (
             <Carousel.Item key={i}>
                 <Image
                     className="d-block w-100"
                     src={image}
                     alt="LEVIN"
+                    onClick={() => {
+                        if(index>24)
+                            history.push('/products/doric');
+                        else if(index%2 === 0)
+                            history.push('/products/doric');
+                        else
+                            history.push('/products/dolby');
+                        }
+                    }
                 />
             </Carousel.Item>
         );
